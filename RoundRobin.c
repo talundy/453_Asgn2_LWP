73ce810ce601a24bbe27cde6795602344f3242cc
talundy <talundy@calpoly.edu>
2025-01-22 22:05:30 -0800

    # wrote rr_next draft

--- RoundRobin.c
+++ RoundRobin.c
@@ -12,9 +12,13 @@ void rr_admit(thread new){
         return;
     }
     if(HEAD == NULL){
+        // Set HEAD to new, point to itself
         HEAD = new;
+        new->next = new;
+        new->prev = new;
         return;
     }
+    // Insert new thread at HEAD->prev
     thread p = HEAD->prev;
     HEAD->prev = new;
     new->prev = p;
@@ -32,7 +36,16 @@ void rr_remove(thread victim){
 }
 
 thread rr_next(void){
-
+    if(HEAD == NULL){
+        return NULL;
+    }
+    // If HEAD is the only thread in the pool
+    if((HEAD->next == HEAD) || (HEAD->next == NULL)){
+        return HEAD;
+    }
+    // Assign HEAD->next as HEAD and return the former head
+    HEAD = HEAD->next;  // revise later
+    return HEAD->prev;
 }
 
 int rr_qlen(void){
