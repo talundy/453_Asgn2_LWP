3666f9c71a0c4813ca5c910007a2b5dae0aca139
tommychobo <thomaschoboter@gmail.com>
2025-01-22 22:16:10 -0800

    qlen()

--- RoundRobin.c
+++ RoundRobin.c
@@ -12,13 +12,9 @@ void rr_admit(thread new){
         return;
     }
     if(HEAD == NULL){
-        // Set HEAD to new, point to itself
         HEAD = new;
-        new->next = new;
-        new->prev = new;
         return;
     }
-    // Insert new thread at HEAD->prev
     thread p = HEAD->prev;
     HEAD->prev = new;
     new->prev = p;
@@ -29,36 +25,30 @@ void rr_admit(thread new){
 
 void rr_remove(thread victim){
     if(victim != NULL){
-        // If victim is the only thread in the pool
-        if((victim->next == victim) && (victim->prev == victim)){
-            // Preferred convention for a single thread pool
-            HEAD = NULL;
-            return;
-        } elif ((victim->next == NULL) && (victim->prev == NULL)){
-            // Convention is this should never happen
-            HEAD = NULL;
-            return;
-        }
         // Change prev and next pointers
         victim->prev->next = victim->next;
         victim->next->prev = victim->prev;
-
     }
 }
 
 thread rr_next(void){
-    if(HEAD == NULL){
-        return NULL;
-    }
-    // If HEAD is the only thread in the pool
-    if((HEAD->next == HEAD) || (HEAD->next == NULL)){
-        return HEAD;
-    }
-    // Assign HEAD->next as HEAD and return the former head
-    HEAD = HEAD->next;  // revise later
-    return HEAD->prev;
+
 }
 
 int rr_qlen(void){
+<<<<<<< HEAD
 
+=======
+    int count = 0;
+    if(HEAD == NULL){
+        return 0;
+    }
+    count++;
+    thread thr = HEAD->next;
+    while(thr != HEAD){
+        thr = thr->next;
+        count++;
+    }
+    return count;
+>>>>>>> ba338d7 (qlen())
 }
